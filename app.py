6861fc4277ae16ca2bf9b5a05bcde49f90d994a7
Serena <serenafgreen@gmail.com>
2025-03-19 17:48:59 -0700

    updates styling

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, request
 from forms import ResumeForm
 from utils import generate_wordclouds, get_word_frequencies, analyze_keywords
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = 'your_secret_key'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,15 +13,18 @@ def index():
         resume_text = form.resume.data
         job_description = form.job_description.data
         resume_wc, job_wc = generate_wordclouds(resume_text, job_description)
-        resume_freq, job_freq = get_word_frequencies(resume_text, job_description)
-        matching_keywords, missing_keywords, match_percentage = analyze_keywords(resume_text, job_description)
-        return render_template('result.html', 
-                              resume_text=resume_text, job_description=job_description,
+        resume_freq, job_freq = get_word_frequencies(
+            resume_text, job_description)
+        matching_keywords, missing_keywords, match_percentage = analyze_keywords(
+            resume_text, job_description)
+        return render_template('result.html',
+                               resume_text=resume_text, job_description=job_description,
 
-                               resume_wc=resume_wc, job_wc=job_wc, 
+                               resume_wc=resume_wc, job_wc=job_wc,
                                resume_freq=resume_freq, job_freq=job_freq,
                                matching_keywords=matching_keywords, missing_keywords=missing_keywords,     match_percentage=match_percentage)
     return render_template('index.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
